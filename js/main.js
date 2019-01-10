d843958e771132d39f1ccf4ed41cbcdc7dbe5c70
Jeriah Bowers <jeriah97bowers@yahoo.com>
2019-01-09 23:11:58 -0500

    Finishing up initial interface change

--- js/main.js
+++ js/main.js
@@ -1,68 +1,26 @@
-const menuButton = document.querySelector(".header__nav--menu");
-const menuList = document.querySelector(".header__nav--list");
-
-
-$(window).on("scroll", function() {
-    if( $(window).scrollTop() > 25 ) {
-       $(".header__nav--container").addClass("nav__active");
-   } else {
-       $(".header__nav--container").removeClass("nav__active");
-   }
-});
-
-
-$(".header__nav--menu").on("click", evt => {
-    if( window.innerWidth < 991 ) {
-        if( menuList.style.display != "flex" ) {
-          $(".header__nav--list").css("display", "flex");
-       } else {
-          $(".header__nav--list").css("display", "none");     
-        }
-     } else if( window.innerWidth > 991 ) {
-        $(".header__nav--list").css("display", "flex");       
+//Adding navscroll functionality
+$(window).on("scroll", () => {
+    if( $(window).scrollTop() > 75 ) {
+        $(".header__nav").addClass("active__nav");
+    } else if( $(window).scrollTop() < 74 ) {
+        $(".header__nav").removeClass("active__nav");
     }
 });
 
-$(window).resize(function() {
-    if( window.innerWidth > 991 ) {
-       menuList.style.display = "flex";
-    } else if( window.innerWidth <= 991 ) {
-       menuList.style.display = "none";      
+//Adding dropdown nav functionality
+$(".mobile__nav img").on("click", () => {
+    if( $(".header__nav--list").css("display") === "none" ) {
+        $(".header__nav--list").css("display", "flex");
+    } else {
+        $(".header__nav--list").css("display", "none");
     }
 });
 
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
+//Checking the windows width so that nav list doesn't disapear on larger screens
+$(window).resize(() => {
+    if( $(window).innerWidth() > 965 ) {
+        $(".header__nav--list").css("display", "flex");
+    } else if( $(window).innerWidth() < 965 ) {
+        $(".header__nav--list").css("display", "none");
     }
-  });
+});
