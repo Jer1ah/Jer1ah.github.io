eccd71c3d13a33e2cf5de7ec35a81077ad153715
Jer1ah <jeriah97bowers@yahoo.com>
2020-06-28 16:51:00 -0400

    changing mobile nav icon and one of the displayed projects

--- js/main.js
+++ js/main.js
@@ -28,43 +28,6 @@ $(window).resize(() => {
 //Adding scroll animations
 AOS.init();
 
-//Adding smooth scrolling
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
-
   //Adding active nav on scroll
   //Adding classes to nav based on position relative to window
   $(window).on("scroll", () => {
@@ -94,7 +57,3 @@ $('a[href*="#"]')
       $(".header__nav--list--items:nth-child(4) a").removeClass("activeScroll");
     }
   });
-
-  function fab(num) {
-    
-  }
