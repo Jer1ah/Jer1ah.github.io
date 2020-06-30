2db5dc364b682d6b86189a289f3b172533864956
Jer1ah <jeriah97bowers@yahoo.com>
2020-06-30 18:57:18 -0400

    changing dropdown navigation and fade in functionality

--- js/main.js
+++ js/main.js
@@ -7,53 +7,55 @@ $(window).on("scroll", () => {
     }
 });
 
-//Adding dropdown nav functionality
-$(".mobile__nav img").on("click", () => {
-    if( $(".header__nav--list").css("display") === "none" ) {
-        $(".header__nav--list").css("display", "flex");
+
+//Adding active nav on scroll
+//Adding classes to nav based on position relative to window
+$(window).on("scroll", () => {
+  if( $("#about").offset().top - $(window).scrollTop() < 65 ) {
+    $(".header__nav--list--items:nth-child(1) a").addClass("activeScroll");
+  } if( $("#skills").offset().top - $(window).scrollTop() < 175 ) {
+    $(".header__nav--list--items:nth-child(2) a").addClass("activeScroll");
+    $(".header__nav--list--items:nth-child(1) a").removeClass("activeScroll");
+  } if( $("#portfolio").offset().top - $(window).scrollTop() < 75 ) {
+    $(".header__nav--list--items:nth-child(3) a").addClass("activeScroll");
+    $(".header__nav--list--items:nth-child(2) a").removeClass("activeScroll");
+  } if( $("#contact").offset().top - $(window).scrollTop() < 275 ) {
+    $(".header__nav--list--items:nth-child(4) a").addClass("activeScroll");
+    $(".header__nav--list--items:nth-child(3) a").removeClass("activeScroll");
+  }
+});
+
+//Removing classes to nav based on position relative to window
+$(window).on("scroll", () => {
+  if($("#about").offset().top - $(window).scrollTop() > 66) {
+    $(".header__nav--list--items:nth-child(1) a").removeClass("activeScroll");
+  } if($("#skills").offset().top - $(window).scrollTop() > 176) {
+    $(".header__nav--list--items:nth-child(2) a").removeClass("activeScroll");
+  } if($("#portfolio").offset().top - $(window).scrollTop() > 76) {
+    $(".header__nav--list--items:nth-child(3) a").removeClass("activeScroll");
+  } if($("#contact").offset().top - $(window).scrollTop() > 275) {
+    $(".header__nav--list--items:nth-child(4) a").removeClass("activeScroll");
+  }
+});
+
+
+//adding mobile-nav functionality
+const mobileNavController = (function() {
+  const _navButton = document.querySelector(".mobile-menu");
+  const _navList = document.querySelector(".header__nav--list");
+  const _mobileBackground = document.querySelector(".mobile-background");
+
+  _navButton.addEventListener("click", () => {
+    if(window.getComputedStyle(_navList).visibility === "hidden") {
+      _mobileBackground.style.transform = "scale(170)";
+      _navList.style.visibility = "visible";
+      _navList.style.opacity = "1";
+      _navButton.src = "img/up-arrow.svg";
     } else {
-        $(".header__nav--list").css("display", "none");
-    }
-});
-
-//Checking the windows width so that nav list doesn't disapear on larger screens
-$(window).resize(() => {
-    if( $(window).innerWidth() > 965 ) {
-        $(".header__nav--list").css("display", "flex");
-    } else if( $(window).innerWidth() < 965 ) {
-        $(".header__nav--list").css("display", "none");
-    }
-});
-
-//Adding scroll animations
-AOS.init();
-
-  //Adding active nav on scroll
-  //Adding classes to nav based on position relative to window
-  $(window).on("scroll", () => {
-    if( $("#about").offset().top - $(window).scrollTop() < 65 ) {
-      $(".header__nav--list--items:nth-child(1) a").addClass("activeScroll");
-    } if( $("#skills").offset().top - $(window).scrollTop() < 175 ) {
-      $(".header__nav--list--items:nth-child(2) a").addClass("activeScroll");
-      $(".header__nav--list--items:nth-child(1) a").removeClass("activeScroll");
-    } if( $("#portfolio").offset().top - $(window).scrollTop() < 75 ) {
-      $(".header__nav--list--items:nth-child(3) a").addClass("activeScroll");
-      $(".header__nav--list--items:nth-child(2) a").removeClass("activeScroll");
-    } if( $("#contact").offset().top - $(window).scrollTop() < 275 ) {
-      $(".header__nav--list--items:nth-child(4) a").addClass("activeScroll");
-      $(".header__nav--list--items:nth-child(3) a").removeClass("activeScroll");
+      _mobileBackground.style.transform = "scale(0)";
+      _navList.style.visibility = "hidden";
+      _navList.style.opacity = "0";
+      _navButton.src = "img/down-arrow.svg";
     }
   });
-
-  //Removing classes to nav based on position relative to window
-  $(window).on("scroll", () => {
-    if($("#about").offset().top - $(window).scrollTop() > 66) {
-      $(".header__nav--list--items:nth-child(1) a").removeClass("activeScroll");
-    } if($("#skills").offset().top - $(window).scrollTop() > 176) {
-      $(".header__nav--list--items:nth-child(2) a").removeClass("activeScroll");
-    } if($("#portfolio").offset().top - $(window).scrollTop() > 76) {
-      $(".header__nav--list--items:nth-child(3) a").removeClass("activeScroll");
-    } if($("#contact").offset().top - $(window).scrollTop() > 275) {
-      $(".header__nav--list--items:nth-child(4) a").removeClass("activeScroll");
-    }
-  });
+}());
