2a6b794a15175a0546840e51005e1420577bc25e
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-11-20 10:27:32 -0500

    rectructuring website

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const portfolioListItemController = (function(){
+  const _listItems = document.querySelectorAll('.portfolio__listItem');
+  const _overlayList = document.querySelectorAll('.overlay');
+
+  for(let i = 0; i < _listItems.length; i++) {
+      _listItems[i].addEventListener('mouseenter', () => {
+          _overlayList[i].style.opacity = '1';
+      });
+
+      _listItems[i].addEventListener('mouseleave', () => {
+          _overlayList[i].style.opacity = '0';
+      });
+  }
+}());
+
+
+const emailButtonController = (function(){
+  const _emailButton = document.querySelector('.contact__email');
+  const _emailIcon = document.querySelector('.contact__email img');
+
+  _emailButton.addEventListener('mouseenter', () => {
+      _emailIcon.src = 'images/email-blue.svg';
+  });
+
+  _emailButton.addEventListener('mouseleave', () => {
+      _emailIcon.src = 'images/email-white.svg';
+  });
+}());
